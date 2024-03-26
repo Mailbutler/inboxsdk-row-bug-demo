a9acb11b4ae16b50fefe9154538323883df1965b
Fabian Jäger <fabian@mailbutler.io>
2024-03-26 13:45:11 +0100

    demo for thread row bug

--- src/content.js
+++ src/content.js
@@ -1,16 +1,10 @@
-import * as InboxSDK from '@inboxsdk/core';
+import * as InboxSDK from "@inboxsdk/core";
 
 InboxSDK.load(2, "Hello World!").then((sdk) => {
-  // the SDK has been loaded, now do something with it!
-  sdk.Compose.registerComposeViewHandler((composeView) => {
-    // a compose view has come into existence, do something with it!
-    composeView.addButton({
-      title: "My Nifty Button!",
-      iconUrl:
-        "https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365",
-      onClick(event) {
-        event.composeView.insertTextIntoBodyAtCursor("Hello World!");
-      },
-    });
+  sdk.Lists.registerThreadRowViewHandler((threadRowView) => {
+    threadRowView.replaceDate({ text: "First" });
+
+    setTimeout(() => threadRowView.replaceDate({ text: "Second" }), 1000);
+    setTimeout(() => threadRowView.replaceDate({ text: "Third" }), 2000);
   });
 });
